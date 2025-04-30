dbb3ff9a13108666a9a9bbbd1817c115c036a57c
nerdymark <nerdymark@gmail.com>
2025-04-30 11:44:26 -0700

    Update code.py

--- code.py
+++ code.py
@@ -16,7 +16,8 @@ and even rows are arranged from right to left.
 import random
 import board  # pylint: disable=import-error
 import neopixel  # pylint: disable=import-error
-from matrix_modules import snake_game, john_conways_game_of_life, flag_wave, dvd_screen_saver
+from matrix_modules import snake_game, john_conways_game_of_life, flag_wave, dvd_screen_saver, \
+    the_matrix, blizzard
 
 # Update this to match the number of NeoPixel LEDs connected to your board.
 NUM_PIXELS = 324
@@ -33,8 +34,10 @@ pixels = neopixel.NeoPixel(
 
 
 while True:
+    blizzard.blizzard(pixels, WIDTH, HEIGHT, delay=0.01, max_frames=1000)
+    the_matrix.the_matrix(pixels, WIDTH, HEIGHT, delay=0.01, max_frames=1000)
     flag_wave.flag_wave(pixels, WIDTH, HEIGHT, delay=0.01, mode="trans", duration=10)
-    snake_game.snake_game(pixels, WIDTH, HEIGHT, delay=0.01, show_log=False)
+    snake_game.snake_game(pixels, WIDTH, HEIGHT, delay=0.0, show_log=False)
     flag_wave.flag_wave(pixels, WIDTH, HEIGHT, delay=0.01, mode="usa", duration=10)
     flag_wave.flag_wave(pixels, WIDTH, HEIGHT, delay=0.01, mode="pride", duration=10)
     dvd_screen_saver.dvd_screen_saver(pixels, WIDTH, HEIGHT, delay=0.01)
@@ -47,3 +50,4 @@ while True:
                                                         show_log=False,
                                                         animations=False,
                                                         max_generations=10)
+
